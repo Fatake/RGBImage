5e65064ac2633601ea7857c4ea8e0ff528a1fbd9
Fatake Raicos <pace_lozano@hotmail.com>
2020-08-17 17:55:00 -0500

    negativos

--- rgb.py
+++ rgb.py
@@ -11,6 +11,8 @@ def main():
     cv2.imshow("Rojo", R)
     cv2.imshow("Verde", G)
     cv2.imshow("Azul", B)
+    
+    negativo(imagen)
 
     #Escala de Grises 
     gris = cv2.cvtColor(imagen, cv2.COLOR_BGR2GRAY)
@@ -18,6 +20,17 @@ def main():
 
     cv2.waitKey(0)
     cv2.destroyAllWindows()
+
+def negativo(imagen):
+    row, col = imagen.shape 
+    negativo = np.zeros((row, col), dtype=np.uint8)
+
+    for a in range(0, row):                                            
+        for b in range(0, col):                                          
+            negativo[a, b] = 255 - img[a, b]        
+
+    cv2.imshow('NEGATIVO', negativo)  
+        
 
 if __name__ == "__main__":
     main()
