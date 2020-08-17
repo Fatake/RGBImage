e03042a6f23ceb472aa57c5bdca023bedac3b5e0
Fatake Raicos <pace_lozano@hotmail.com>
2020-08-17 17:37:50 -0500

    Se agrega imagenes en 1012 y 512 pixeles

---
+++ mascara.py
@@ -0,0 +1,31 @@
+import numpy as np
+import cv2
+
+IMAGE_NAME = "img/img_512.jpg"
+
+imagen = cv2.imread(IMAGE_NAME)
+cv2.imshow("Original",imagen)
+
+# mismo alto y ancho de la imagen 
+mask = np.zeros(imagen.shape[:2], dtype="uint8")
+
+# Se busca el centro de la imagen // integer division
+(cx, cy) = (imagen.shape[1] // 2, imagen.shape[0] // 2)
+# Se dibuja un rectangulo del tamaño de la mascara
+cv2.rectangle(mask, (cx-75, cy-75), (cx+75 , cy+75), 255, -1)
+cv2.imshow("Mascara",mask)
+
+masked = cv2.bitwise_and(imagen, imagen, mask= mask)
+cv2.imshow("Mascara Aplicada", masked)
+
+
+mask = np.zeros(imagen.shape[:2], dtype ="uint8")
+cv2.circle(mask, (cx, cy), 100, 255, -1)
+
+masked = cv2.bitwise_and(imagen, imagen, mask = mask)
+cv2.imshow("Mask", mask)
+cv2.imshow("Mascara Aplicada Circulo", masked)
+
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
--- rgb.py
+++ rgb.py
@@ -1,5 +1,23 @@
 import cv2
 import numpy as np
 
-IMAGE_NAME = "img/img1.jpg"
-img = cv2.imread('trump.jpg')
+def main():
+    IMAGE_NAME = "img/img_512.jpg"
+
+    imagen = cv2.imread(IMAGE_NAME,1)
+    cv2.imshow("Original",imagen)
+
+    (B, G, R) = cv2.split(imagen)
+    cv2.imshow("Rojo", R)
+    cv2.imshow("Verde", G)
+    cv2.imshow("Azul", B)
+
+    #Escala de Grises 
+    gris = cv2.cvtColor(imagen, cv2.COLOR_BGR2GRAY)
+    cv2.imshow('gris',gris)
+
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+if __name__ == "__main__":
+    main()
